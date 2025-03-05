f87b80eb41bc15a5a26bc853ddb30714c8413c51
Dhifaoui Iyed <dhifaoui.iyed@gmail.com>
2025-03-05 22:34:50 +0100

    add the commments to the ServerScocket code

--- Socket/src/EX1/SocketServeur.java
+++ Socket/src/EX1/SocketServeur.java
@@ -8,7 +8,7 @@ public class SocketServeur {
 	public static void main(String argv[]) {
 		int port = 0;
 		Scanner keyb = new Scanner(System.in);
-		// .............................................................
+		// Demande à l'utilisateur de saisir le port d'écoute
 		System.out.print("Port d'écoute : ");
 		try {
 		port = keyb.nextInt();
@@ -18,24 +18,24 @@ public class SocketServeur {
 		System.exit(-1);
 		}
 		try {
-		// .............................................................
+		// Création du serveur socket écoutant sur le port spécifié
 		ServerSocket serverSocket = new ServerSocket(port);
-		// ..........................................................
+		// Attente d'une connexion client (bloquant)
 
 		Socket socket = serverSocket.accept();
 
-		// ..........................................................
+		//  Création des flux pour envoyer et recevoir des objets
 		ObjectOutputStream output =
 		new ObjectOutputStream(socket.getOutputStream());
 		ObjectInputStream input =
 		new ObjectInputStream(socket.getInputStream());
-		// ..........................................................
+		// Lecture d'un message envoyé par le client
 		String chaine = (String) input.readObject();
 		System.out.println(" recu : " + chaine);
-		// ..........................................................
+		// Affichage de l'adresse et du port du client
 		System.out.println(" ca vient de : " + socket.getInetAddress() +
 		":" + socket.getPort());
-		// ..........................................................
+		// Envoi d'un accusé de réception au client
 		output.writeObject(new String("bien recu"));
 		} catch (Exception e) {
 		System.err.println("Erreur : " + e);
